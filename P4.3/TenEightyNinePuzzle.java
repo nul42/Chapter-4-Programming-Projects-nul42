29ecb64064ec226f40ccfdbafc0f5507bd2a2ef2
nul42 <58828561+nul42@users.noreply.github.com>
2021-02-17 09:48:35 -0600

    Update TenEightyNinePuzzle.java

--- P4.3/TenEightyNinePuzzle.java
+++ P4.3/TenEightyNinePuzzle.java
@@ -1,3 +1,4 @@
+package p4_3;
 
 import java.util.Scanner;
 
@@ -5,12 +6,15 @@ import java.util.Scanner;
  * Read a number from the user. Discard all but the last three digits.
  * Reverse the digits, subtract the original from the reversed (discarding any
  * minus sign), reverse the digits of the difference, and add the difference and
- * the reversed difference. For example: Input: 371 Reversed: 173 Difference:
- * 198 Reversed: 891 Sum: 1089
- *
+ * the reversed difference. For example:
+ * 
+ * Input: 371
+ * Reversed: 173
+ * Difference: 198
+ * Reversed: 891
+ * Sum: 1089
  */
-public class TenEightyNinePuzzle.java
-{
+public class TenEightyNinePuzzle {
 
     /**
      * Reverse the last 3 digits of number
@@ -18,17 +22,42 @@ public class TenEightyNinePuzzle.java
      * @param n the number
      * @return the reverse of the last 3 digits of n
      */
-    static int reverse(int n)
-    {
-
+    static int reverse(int n) {
+//    	n = n % 1000;
+    	
+    	char first = (n+"").charAt(0);
+    	char second = (n+"").charAt(1);
+    	char third = (n+"").charAt(2);
+//		System.out.println(third+second+first);//I believe it was grabbing the character numerical values and adding them.
+		return Integer.valueOf(third+""+second+""+first);
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-      
+    public static void main(String[] args) {
+//        int input = new Scanner(System.in).nextInt();//Resource Leak
+    	
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+    	
+//    	input = 3282371;
+    	input = Math.abs(input % 1000);
+    	int reversed1 = reverse(input);
+    	int difference = Math.abs(input - reversed1);
+    	int reversed2 = reverse(difference);
+    	int sum = difference + reversed2;
+    	
+//    	System.out.println("Input was: "+input);
+    	System.out.println(reversed1);
+    	System.out.println("Expected: 173");
+    	System.out.println(difference);
+    	System.out.println("Expected: 198");
+    	System.out.println(reversed2);
+    	System.out.println("Expected: 891");
+    	System.out.println(sum);
+    	System.out.println("Expected: 1089");
     }
 }
 
